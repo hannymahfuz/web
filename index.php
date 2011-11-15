4a998e4fdab676ce46365711444c0dcdb62a4deb
Hanny Mahfuz <mahakasih@gmail.com>
2011-11-15 11:37:40 +0700

    index nya

---
+++ index.php
@@ -0,0 +1,18 @@
+<head>
+	<link href="lib/css/style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="lib/js/javascript.js"> </script>
+</head>
+
+<body>
+	<div id="container">
+		<!-- conten nya ini -->
+		<div id="header">
+			<h1> <a href="#">Creativex</a></h1>
+				<ul>
+					<li id="home"><a href="home">home</a></li>
+					<li id="link"><a href="link">link</a></li>
+					<li id="aboutus"><a href="aboutus">about us</a></li>
+				</ul>
+		</div>
+	</div>
+</body>
